2cfc1e6daf54b8345f1beb8dff9297d2f79e65c7
zhabwe254 <zhabwe@gmail.com>
2024-04-29 23:17:08 +0300

    a

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,9 +1,17 @@
-from flask import Flask, Blueprint, jsonify
+from flask import jsonify
+from api.v1.views import app_views
+from models import storage
 
-app_views = Blueprint('app_views', __name__)
 
-
-@app_views.route('/status', methods=['GET'], strict_slashes=False)
-def status():
-    """Returns the status of the API"""
-    return jsonify({"status": "OK"})
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def get_stats():
+    """Returns the number of each object type"""
+    stats = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+    }
+    return jsonify(stats)
