f534b76055b348cb843d3b5330f896a32a7c8c6e
zhabwe254 <zhabwe@gmail.com>
2024-04-30 11:54:49 +0300

    a

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -1,11 +1,55 @@
 import unittest
 from models.engine.db_storage import DBStorage
+from models.state import State
+from models.user import User
+from models.city import City
+from models.base_model import BaseModel
 
-class TestDBStorage(unittest.TestCase):
-    def test_get_method(self):
-        """Test get method"""
-        pass
+class TestStorageGet(unittest.TestCase):
+    """
+    Testing `get()` method in DBStorage
+    """
 
-    def test_count_method(self):
-        """Test count method"""
-        pass
+    @classmethod
+    def setUpClass(cls):
+        """
+        setup tests for class
+        """
+        print('\n\n.................................')
+        print('...... Testing Get() Method ......')
+        print('.......... Place  Class ..........')
+        print('.................................\n\n')
+
+    def setUp(self):
+        """
+        setup method
+        """
+        self.storage = DBStorage()
+
+    def test_get_method_obj(self):
+        """
+        testing get() method
+        :return: True if pass, False if not pass
+        """
+        state = State(name="Florida")
+        state.save()
+        result = self.storage.get(cls="State", id=state.id)
+        self.assertIsInstance(result, State)
+
+    def test_get_method_return(self):
+        """
+        testing get() method for id match
+        :return: True if pass, false if not pass
+        """
+        state = State(name="Florida")
+        state.save()
+        result = self.storage.get(cls="State", id=str(state.id))
+        self.assertEqual(state.id, result.id)
+
+    def test_get_method_none(self):
+        """
+        testing get() method for None return
+        :return: True if pass, false if not pass
+        """
+        result = self.storage.get(cls="State", id="doesnotexist")
+        self.assertIsNone(result)
