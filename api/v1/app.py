1250c6ae646dcf37b34f5ccea118858927e1c975
zhabwe254 <zhabwe@gmail.com>
2024-04-30 15:38:27 +0300

    s

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,21 +1,37 @@
 #!/usr/bin/python3
 """
-Flask application
+app
 """
-from api.v1.views import app_views
+
 from flask import Flask, jsonify
+from flask_cors import CORS
+from os import getenv
+from api.v1.views import app_views
+from models import storage
 
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 app.register_blueprint(app_views)
-app.url_map.strict_slashes = False
-app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+
+@app.teardown_appcontext
+def teardown(exception):
+    """
+    teardown function
+    """
+    storage.close()
 
 @app.errorhandler(404)
-def not_found(error):
+def handle_404(exception):
     """
-    Handle 404 errors
+    handles 404 error
+    :return: returns 404 json
     """
-    return jsonify({"error": "Not found"}), 404
+    data = {
+        "error": "Not found"
+    }
+    resp = jsonify(data)
+    resp.status_code = 404
+    return resp
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, threaded=True)
+    app.run(getenv("HBNB_API_HOST"), getenv("HBNB_API_PORT"))
