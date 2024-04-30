386bae5300f916b54b2ad51cf2ac354bf5e0dd56
zhabwe254 <zhabwe@gmail.com>
2024-04-30 11:01:03 +0300

    p

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,48 +1,78 @@
+# models/engine/db_storage.py
+
 from models.base_model import BaseModel, Base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 from os import getenv
 
 class DBStorage:
+    """This class manages storage using SQLAlchemy ORM."""
+
     __engine = None
     __session = None
 
     def __init__(self):
-        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'
-                                        .format(getenv('HBNB_MYSQL_USER'),
-                                                getenv('HBNB_MYSQL_PWD'),
-                                                getenv('HBNB_MYSQL_HOST'),
-                                                getenv('HBNB_MYSQL_DB')),
-                                        pool_pre_ping=True)
+        """Initialize DBStorage."""
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.
+                                      format(getenv('HBNB_MYSQL_USER'),
+                                             getenv('HBNB_MYSQL_PWD'),
+                                             getenv('HBNB_MYSQL_HOST'),
+                                             getenv('HBNB_MYSQL_DB')),
+                                      pool_pre_ping=True)
         if getenv('HBNB_ENV') == 'test':
             Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
-        pass
+        """Query all objects of a certain class."""
+        obj_dict = {}
+        classes = [cls] if cls else [BaseModel, User, State, City, Amenity, Place, Review]
+        for cls in classes:
+            objs = self.__session.query(cls).all()
+            for obj in objs:
+                key = "{}.{}".format(obj.__class__.__name__, obj.id)
+                obj_dict[key] = obj
+        return obj_dict
 
     def new(self, obj):
-        pass
+        """Add object to current database session."""
+        self.__session.add(obj)
 
     def save(self):
-        pass
+        """Commit all changes to current database session."""
+        self.__session.commit()
 
     def delete(self, obj=None):
-        pass
+        """Delete object from current database session."""
+        if obj:
+            self.__session.delete(obj)
 
     def reload(self):
+        """Create all tables in the database."""
         Base.metadata.create_all(self.__engine)
-        session_factory = sessionmaker(bind=self.__engine,
-                                        expire_on_commit=False)
+        session_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
         Session = scoped_session(session_factory)
         self.__session = Session()
 
     def close(self):
+        """Close the current session."""
         self.__session.close()
 
     def get(self, cls, id):
-        """Retrieve one object"""
-        pass
+        """Retrieve one object based on class and id."""
+        try:
+            return self.__session.query(cls).filter_by(id=id).one()
+        except NoResultFound:
+            return None
 
     def count(self, cls=None):
-        """Count the number of objects in storage"""
-        pass
+        """Count the number of objects in storage."""
+        if cls:
+            return self.__session.query(cls).count()
+        else:
+            total_count = 0
+            classes = [BaseModel, User, State, City, Amenity, Place, Review]
+            for cls in classes:
+                total_count += self.__session.query(cls).count()
+            return total_count
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,29 +1,39 @@
+# models/engine/file_storage.py
+
 import json
 from models.base_model import BaseModel
 
 class FileStorage:
+    """This class manages storage using JSON file."""
+
     __file_path = "file.json"
     __objects = {}
 
     def all(self, cls=None):
-        pass
+        """Return dictionary of all objects."""
+        if cls:
+            return {k: v for k, v in self.__objects.items() if isinstance(v, cls)}
+        else:
+            return self.__objects
 
     def new(self, obj):
-        pass
+        """Add object to __objects."""
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        self.__objects[key] = obj
 
     def save(self):
-        pass
+        """Save objects to JSON file."""
+        with open(self.__file_path, 'w') as f:
+            json.dump({k: v.to_dict() for k, v in self.__objects.items()}, f)
 
     def reload(self):
-        pass
-
-    def delete(self, obj=None):
-        pass
-
-    def get(self, cls, id):
-        """Retrieve one object"""
-        pass
-
-    def count(self, cls=None):
-        """Count the number of objects in storage"""
-        pass
+        """Load objects from JSON file."""
+        try:
+            with open(self.__file_path, 'r') as f:
+                data = json.load(f)
+                for k, v in data.items():
+                    cls_name, obj_id = k.split('.')
+                    cls = eval(cls_name)
+                    self.__objects[k] = cls(**v)
+        except FileNotFoundError:
+            pass
