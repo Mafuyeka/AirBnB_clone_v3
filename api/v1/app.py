8aa3ba51d12e83ccfc6f7df1d4aace86728af2d3
tshepisoroshina@gmail.com <tshepisoroshina@gmail.com>
2024-04-30 08:22:58 -0700

    Updated

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,81 +1,41 @@
 #!/usr/bin/python3
+"""Flask web server to handle API requests.
+
+This module sets up a Flask web server that handles API requests.
+It registers the necessary routes and configurations
+to respond to HTTP requests.
+The server listens on the specified host and port,
+which can be customized through environment variables.
 """
-<<<<<<< HEAD
-This module contains the principal application
-"""
-from models import storage
+
 from api.v1.views import app_views
-from flask import Flask, make_response, jsonify
-from os import getenv
+from flask import Blueprint, Flask, jsonify
 from flask_cors import CORS
-from flasgger import Swagger
+from models import storage
+from os import getenv
+
 
 app = Flask(__name__)
-app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.register_blueprint(app_views)
-cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
+app.url_map.strict_slashes = False
+
+CORS(app, origins="0.0.0.0")
+
+api_host = getenv("HBNB_API_HOST", "0.0.0.0")
+api_port = getenv("HBNB_API_PORT", 5000)
 
 
 @app.teardown_appcontext
-def close_db(obj):
-    """ calls methods close() """
+def teardown(self):
+    """Closes the database storage connection."""
     storage.close()
 
 
 @app.errorhandler(404)
-def page_not_foun(error):
-    """ Loads a custom 404 page not found """
-    return make_response(jsonify({"error": "Not found"}), 404)
+def not_found(error):
+    """Handles 404 errors and returns a JSON-formatted 404 response."""
+    return jsonify({"error": "Not found"}), 404
 
-
-app.config['SWAGGER'] = {
-    'title': 'AirBnB clone - RESTful API',
-    'description': 'This is the api that was created for the hbnb restful api project,\
-    all the documentation will be shown below',
-    'uiversion': 3}
-
-Swagger(app)
 
 if __name__ == "__main__":
-
-    host = getenv('HBNB_API_HOST', default='0.0.0.0')
-    port = getenv('HBNB_API_PORT', default=5000)
-
-    app.run(host, int(port), threaded=True)
-=======
-app
-"""
-
-from flask import Flask, jsonify
-from flask_cors import CORS
-from os import getenv
-from api.v1.views import app_views
-from models import storage
-
-app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
-app.register_blueprint(app_views)
-
-@app.teardown_appcontext
-def teardown(exception):
-    """
-    teardown function
-    """
-    storage.close()
-
-@app.errorhandler(404)
-def handle_404(exception):
-    """
-    handles 404 error
-    :return: returns 404 json
-    """
-    data = {
-        "error": "Not found"
-    }
-    resp = jsonify(data)
-    resp.status_code = 404
-    return resp
-
-if __name__ == "__main__":
-    app.run(getenv("HBNB_API_HOST"), getenv("HBNB_API_PORT"))
->>>>>>> 03aa4930f057d140fd8e80803821df7b6fe16c03
+    app.run(host=api_host, port=int(api_port), threaded=True)
